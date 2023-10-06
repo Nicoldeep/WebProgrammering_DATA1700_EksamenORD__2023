e3291a71bd3466942e19aae27fb85a9d7355d423
Nicoldeep <nicoldeep@gmail.com>
2023-10-06 12:10:23 +0200

    Lager prosjekt

---
+++ src/main/java/com/example/ordeksamen2023/OrdEksamen2023Application.java
@@ -0,0 +1,13 @@
+package com.example.ordeksamen2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdEksamen2023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrdEksamen2023Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ordeksamen2023/OrdEksamen2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ordeksamen2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdEksamen2023ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
